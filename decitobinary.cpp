ecb4447d7187054f44095cb0ade5210bb36367de
Sachin Negi <negisachin750@gmail.com>
2022-11-27 16:44:48 +0530

    commit

---
+++ decitobinary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int decimaltobinary(int n){
+    int ans=0;
+    int x=1;
+    while(x <=n){
+        x*=2;
+    }
+    x/=2;
+    while(x>0){
+        int lastdigit=n/x;
+        n-=lastdigit*x;
+        x/=2;
+        ans=ans*10+lastdigit;
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"enter the decimal : ";
+    cin>>n;
+    cout<<"binary value of decimal is : "<<decimaltobinary(n)<<endl;
+}
