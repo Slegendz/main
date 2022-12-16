91caab4ae7663f848a500fcd8a66bc7e1b216faf
Sachin Negi <negisachin750@gmail.com>
2022-12-16 17:25:35 +0530

    commit

--- array1.cpp
+++ array1.cpp
@@ -16,9 +16,13 @@ int main(){
      int smallest=INT_MAX;
 
      for(int i=0;i<n;i++){
-        greatest=max(greatest,arr[i]);
-        smallest=min(smallest,arr[i]);
+        if(arr[i]>greatest){
+            greatest=arr[i];
+        }
+        if(arr[i]<smallest){
+            smallest=arr[i];
+        }
      }
      cout<<"max value is : "<<greatest<<endl;;
-     cout<<"min value is : "<<smallest;
+     cout<<"min value is : \n"<<smallest;
 }
